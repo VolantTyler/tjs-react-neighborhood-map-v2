b6d0759445dff943cffe0c4e38a86ce3f929b7e0
VolantTyler <33227593+VolantTyler@users.noreply.github.com>
2018-10-02 11:16:24 -0400

    feat: Foursquare API set up

--- src/App.js
+++ src/App.js
@@ -3,15 +3,38 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import axios from 'axios'
+
 class App extends Component {
 
   componentDidMount() {
+    this.getVenues()
     this.renderMap()
   }
 
   renderMap = () => {
     loadScript('https://maps.googleapis.com/maps/api/js?key=AIzaSyAVcvtwNGlsHF1Rqayx3Mbzneiz_4dUBzc&callback=initMap')
     window.initMap = this.initMap
+  }
+
+  getVenues = () => {
+    const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
+    const parameters = {
+      client_id: 'M3LJTA2QWGOG2RY4DVF3LUW3MCQK2UKCHA3WJ0LWKPIDMX0M',
+      client_secret: '30E5HRSYMOK2XHIROYC0CCOOCVWYSHK4HAEHNPAG5PPJJCUH',
+      query: 'burger',
+      near: 'Ridgewood, NJ',
+      //ll: '44.3,37.2',
+      v: '20181001'
+    }
+
+    axios.get(endPoint + new URLSearchParams(parameters))
+      .then(response => {
+        console.log(response)
+      })
+      .catch(error => {
+        console.log('Error: '+error)
+      })
   }
 
   initMap = () => {
